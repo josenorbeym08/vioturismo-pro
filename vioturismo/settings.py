89f6b737f763bc8f6e8043a38cde6a7547078735
vioturismo <josenorbey-08@hotmail.es>
2015-06-05 09:52:45 -0500

    subida de archivos 14 de marzo

--- vioturismo/settings.py
+++ vioturismo/settings.py
@@ -160,3 +160,30 @@ EMAIL_USE_TLS = True
 
 
 URL_LOGIN = '/login/'
+
+
+
+#Para cuando corra collestatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    BASE_DIR + '/static',
+)
+
+TEMPLATE_DIRS = (
+    BASE_DIR + '/templates/',
+)
+
+#Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
